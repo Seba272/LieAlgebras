e06b2a1d2a7afcb5b7fbf2d50cbef3d0c84a41fd
Seba272 <git@sebang.fastmail.fm>
2022-12-22 17:52:52 +0100

    Add extras.

--- LieAlgebras5_Extras.py
+++ LieAlgebras5_Extras.py
@@ -75,3 +75,79 @@ def left_invariant_coframe(la:LieAlgebra,p=None):
     coframe0 = eye(dim).rowspace()
     coframe = [v.multiply(DLxinv_atx) for v in coframe0]
     return coframe
+
+def D_left_tranaslation(la:LieAlgebra,p,q=None):
+    """Returns the differential at q of the left-translation by p.
+    
+    """
+    dim = la.dimension
+    x = la.a_vector_array('x')
+    y = la.a_vector_array('y')
+    _p = p.copy()
+    if q == None:
+        _q = 0*x
+    else:
+        _q = q.copy()
+    x_to_p = {x[j,0]:_p[j,0] for j in range(dim)}
+    y_to_q = {y[j,0]:_q[j,0] for j in range(dim)}
+    xy = la.bch(x,y)
+    Dxy = diff(xy,y).reshape(dim,dim).transpose()
+    DLp_at_q = Dxy.subs(x_to_p).subs(y_to_q)
+    return DLp_at_q
+
+# For blow-up of intrinsic graphs:
+# implement:
+# \delta_t( f(w)^{-1} f( w f(w) (\delta_{1/t}u) f(w)^{-1} ) )
+def ff(la,f,t,w,u):
+    res = la.dil(1/t,u)
+    right = -f(w)
+    left = la.bch(w,f(w))
+    res = la.bch(res,right)
+    res = la.bch(left,res)
+    res = f(res)
+    left = right
+    res = la.bch(left,res)
+    res = la.dil(t,res)
+    return res
+
+def show_bra(la):
+    basis = la.basis_symbolic
+    dim = la.dimension
+    M = zeros(dim+1,dim+1)
+    for i in range(dim):
+        M[i+1,0] = M[0,i+1] = basis[i]
+    for i in range(dim):
+        for j in range(dim):
+    #         print(i,j)
+            M[i+1,j+1] = la(basis[i],basis[j])
+    display(M)
+    
+from tabulate import tabulate
+
+def show_bra_tab(la):
+    basis = la.basis_symbolic
+    dim = la.dimension
+    M = zeros(dim+1,dim+1)
+    for i in range(dim):
+        M[i+1,0] = M[0,i+1] = basis[i]
+    for i in range(dim):
+        for j in range(dim):
+    #         print(i,j)
+            M[i+1,j+1] = la(basis[i],basis[j])
+
+    M_list = [[M[i,j] for j in range(M.cols)] for i in range(M.rows)]
+    tabella = tabulate(M_list,headers="firstrow", tablefmt="html")
+    display(tabella)
+    
+def export_bra(la):
+    basis = la.basis_symbolic
+    dim = la.dimension
+    list_bras = []
+    for j in range(dim):
+        for i in range(j):
+            a = basis[i]
+            b = basis[j]
+            ab = la(a,b)
+            if ab != 0*a:
+                list_bras.append('(' + str(a) + ', ' + str(b) + ') :' + str(ab))
+    return list_bras
